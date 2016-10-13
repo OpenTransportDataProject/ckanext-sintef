f454dd775cb419b3daf55c96f79a99ec08567f99
Awat A. Langø <awat.langoe@gmail.com>
2016-10-13 12:39:32 +0200

    import_stage() under review. base.py created.

--- ckanext/sintef/harvesters/geonorgeharvester.py
+++ ckanext/sintef/harvesters/geonorgeharvester.py
@@ -21,7 +21,9 @@ from ckanext.harvest.model import HarvestJob, HarvestObject, HarvestGatherError
 import logging
 log = logging.getLogger(__name__)
 
-class GeonorgeHarvester(SingletonPlugin):
+from ckanext.harvest.harvesters.base import HarvesterBase
+
+class GeonorgeHarvester(HarvesterBase):
     '''
     Geonorge Harvester
     '''
@@ -295,6 +297,182 @@ class GeonorgeHarvester(SingletonPlugin):
                   need harvesting after all or False if there were errors.
         '''
         log.debug('In GeonorgeHarvester import_stage')
+
+        log.debug(harvest_object.job.source.title)
+
+        base_context = {'model': model, 'session': model.Session,
+                        'user': None}
+        if not harvest_object:
+            log.error('No harvest object received')
+            return False
+
+        if harvest_object.content is None:
+            self._save_object_error('Empty content for object %s' %
+                                    harvest_object.id,
+                                    harvest_object, 'Import')
+            return False
+
+        self._set_config(harvest_object.job.source.config)
+
+        try:
+            package_dict = json.loads(harvest_object.content)
+
+            package_dict['id'] = package_dict.pop('Uuid')
+            package_dict['title'] = package_dict.pop('Title')
+
+            if package_dict.get('type') == 'harvest':
+                log.warn('Remote dataset is a harvest source, ignoring...')
+                return True
+
+            # Set default tags if needed
+            default_tags = self.config.get('default_tags', [])
+            if default_tags:
+                if not 'tags' in package_dict:
+                    package_dict['tags'] = []
+                package_dict['tags'].extend(
+                    [t for t in default_tags if t not in package_dict['tags']])
+
+            remote_groups = self.config.get('remote_groups', None)
+            if not remote_groups in ('only_local', 'create'):
+                # Ignore remote groups
+                package_dict.pop('groups', None)
+            else:
+                if not 'groups' in package_dict:
+                    package_dict['groups'] = []
+
+                # check if remote groups exist locally, otherwise remove
+                validated_groups = []
+
+                for group_ in package_dict['groups']:
+                    try:
+                        data_dict = {'id': group_['id']}
+                        group = get_action('group_show')(base_context.copy(), data_dict)
+                        validated_groups.append({'id': group['id'], 'name': group['name']})
+
+                    except NotFound, e:
+                        log.info('Group %s is not available', group_)
+                        if remote_groups == 'create':
+                            try:
+                                group = self._get_group(harvest_object.source.url, group_)
+                            except RemoteResourceError:
+                                log.error('Could not get remote group %s', group_)
+                                continue
+
+                            for key in ['packages', 'created', 'users', 'groups', 'tags', 'extras', 'display_name']:
+                                group.pop(key, None)
+
+                            get_action('group_create')(base_context.copy(), group)
+                            log.info('Group %s has been newly created', group_)
+                            validated_groups.append({'id': group['id'], 'name': group['name']})
+
+                package_dict['groups'] = validated_groups
+
+            # Local harvest source organization
+            source_dataset = get_action('package_show')(base_context.copy(), {'id': harvest_object.source.id})
+            local_org = source_dataset.get('owner_org')
+
+            remote_orgs = self.config.get('remote_orgs', None)
+
+            if not remote_orgs in ('only_local', 'create'):
+                # Assign dataset to the source organization
+                package_dict['owner_org'] = local_org
+            else:
+                if not 'owner_org' in package_dict:
+                    package_dict['owner_org'] = None
+
+                # check if remote org exist locally, otherwise remove
+                validated_org = None
+                remote_org = package_dict['owner_org']
+
+                if remote_org:
+                    try:
+                        data_dict = {'id': remote_org}
+                        org = get_action('organization_show')(base_context.copy(), data_dict)
+                        validated_org = org['id']
+                    except NotFound, e:
+                        log.info('Organization %s is not available', remote_org)
+                        if remote_orgs == 'create':
+                            try:
+                                try:
+                                    org = self._get_organization(harvest_object.source.url, remote_org)
+                                except RemoteResourceError:
+                                    # fallback if remote CKAN exposes organizations as groups
+                                    # this especially targets older versions of CKAN
+                                    org = self._get_group(harvest_object.source.url, remote_org)
+
+                                for key in ['packages', 'created', 'users', 'groups', 'tags', 'extras', 'display_name', 'type']:
+                                    org.pop(key, None)
+                                get_action('organization_create')(base_context.copy(), org)
+                                log.info('Organization %s has been newly created', remote_org)
+                                validated_org = org['id']
+                            except (RemoteResourceError, ValidationError):
+                                log.error('Could not get remote org %s', remote_org)
+
+                package_dict['owner_org'] = validated_org or local_org
+
+            # Set default groups if needed
+            default_groups = self.config.get('default_groups', [])
+            if default_groups:
+                if not 'groups' in package_dict:
+                    package_dict['groups'] = []
+                existing_group_ids = [g['id'] for g in package_dict['groups']]
+                package_dict['groups'].extend(
+                    [g for g in self.config['default_group_dicts']
+                     if g['id'] not in existing_group_ids])
+
+            # Set default extras if needed
+            default_extras = self.config.get('default_extras', {})
+            def get_extra(key, package_dict):
+                for extra in package_dict.get('extras', []):
+                    if extra['key'] == key:
+                        return extra
+            if default_extras:
+                override_extras = self.config.get('override_extras', False)
+                if not 'extras' in package_dict:
+                    package_dict['extras'] = {}
+                for key, value in default_extras.iteritems():
+                    existing_extra = get_extra(key, package_dict)
+                    if existing_extra and not override_extras:
+                        continue  # no need for the default
+                    if existing_extra:
+                        package_dict['extras'].remove(existing_extra)
+                    # Look for replacement strings
+                    if isinstance(value, basestring):
+                        value = value.format(
+                            harvest_source_id=harvest_object.job.source.id,
+                            harvest_source_url=
+                            harvest_object.job.source.url.strip('/'),
+                            harvest_source_title=
+                            harvest_object.job.source.title,
+                            harvest_job_id=harvest_object.job.id,
+                            harvest_object_id=harvest_object.id,
+                            dataset_id=package_dict['id'])
+
+                    package_dict['extras'].append({'key': key, 'value': value})
+
+            for resource in package_dict.get('resources', []):
+                # Clear remote url_type for resources (eg datastore, upload) as
+                # we are only creating normal resources with links to the
+                # remote ones
+                resource.pop('url_type', None)
+
+                # Clear revision_id as the revision won't exist on this CKAN
+                # and saving it will cause an IntegrityError with the foreign
+                # key.
+                resource.pop('revision_id', None)
+
+            result = self._create_or_update_package(
+                package_dict, harvest_object, package_dict_form='package_show')
+
+            return result
+        except ValidationError, e:
+            self._save_object_error('Invalid package with GUID %s: %r' %
+                                    (harvest_object.guid, e.error_dict),
+                                    harvest_object, 'Import')
+            log.error(e.error_dict)
+        except Exception, e:
+            #self._save_object_error('%s' % e, harvest_object, 'Import')
+            log.error(e)
 
     def _search_for_datasets(self, remote_geonorge_base_url):
         base_search_url = remote_geonorge_base_url + self._get_search_api_offset()
