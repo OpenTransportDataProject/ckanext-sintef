f04e561ea75d67b887a67aff3b23d785791d31e1
henriksunde <henriksunde@hotmail.com>
2016-10-20 13:38:38 +0200

    Removed hardcoded 'type' from config-

--- ckanext/sintef/harvesters/geonorgeharvester.py
+++ ckanext/sintef/harvesters/geonorgeharvester.py
@@ -7,7 +7,6 @@ import urllib2
 import httplib
 import datetime
 import socket
-import re
 
 from sqlalchemy import exists
 
@@ -73,21 +72,6 @@ class GeonorgeHarvester(HarvesterBase):
             'title': 'Geonorge Server',
             'description': 'Harvests from Geonorge instances.'
         }
-
-    def _make_lower_and_alphanumeric(self, s):
-        s_dict = {' ': '-',
-                  u'\u00E6': 'ae',
-                  u'\u00C6': 'ae',
-                  u'\u00F8': 'oe',
-                  u'\u00D8': 'oe',
-                  u'\u00E5': 'aa',
-                  u'\u00C5': 'aa'}
-
-        for key in s_dict:
-            s = s.replace(key, s_dict[key])
-
-        s = s.lower()
-        return re.sub(r'[^A-Za-z0-9\-\_]+', '', s)
 
     def _set_config(self, config_str):
         if config_str:
@@ -450,13 +434,13 @@ class GeonorgeHarvester(HarvesterBase):
             package_dict['notes'] = package_dict.pop('Abstract')
             package_dict['url'] = package_dict.pop('ShowDetailsUrl')
             package_dict['isopen'] = package_dict.pop('IsOpenData')
-
-            organization_name = package_dict.pop('Organization')
-            package_dict['owner_org_name'] = organization_name
-            package_dict['owner_org'] = self._make_lower_and_alphanumeric(organization_name)
+            package_dict['owner_org'] = package_dict.pop('Organization').lower()
 
             package_dict['tags'] = []
-            package_dict['tags'].append({'name': package_dict.pop('Theme')})
+            info = {
+                    'name': package_dict.pop('Theme')
+                    }
+            package_dict['tags'].append(info)
 
             if package_dict.get('type') == 'harvest':
                 log.warn('Remote dataset is a harvest source, ignoring...')
@@ -531,9 +515,15 @@ class GeonorgeHarvester(HarvesterBase):
                         log.info('Organization %s is not available', remote_org)
                         if remote_orgs == 'create':
                             try:
-                                org = {'name': package_dict['owner_org'],
-                                       'title': package_dict['owner_org_name']}
+                                org = {'name': package_dict['owner_org']}
 
+                                log.debug('ORG DICT: %s', org)
+
+                                # org['users'] = []
+                                # org['users'].append({'name': '9015271d-80e3-4f63-a356-22415825a746'})
+
+                                # for key in ['packages', 'created', 'users', 'groups', 'tags', 'extras', 'display_name', 'type']:
+                                #     org.pop(key, None)
                                 get_action('organization_create')(base_context.copy(), org)
                                 log.info('Organization %s has been newly created', remote_org)
                                 validated_org = org['id']
@@ -613,15 +603,15 @@ class GeonorgeHarvester(HarvesterBase):
         fq_term_counter = 0
         for fq_term in fq_terms:
             params.update({'facets[' + str(fq_term_counter) + ']name': fq_term})
-            params.update({'facets[' + str(fq_term_counter) + ']value': fq_terms[fq_term]})
+            params.update({'facets[' + str(fq_term_counter) + ']value': "%s" % (fq_terms[fq_term])})
             fq_term_counter += 1
         param_keys_sorted = sorted(params)
         pkg_dicts = []
 
         while True:
-            url = base_search_url + '?'# + urllib.urlencode(params)
+            url = base_search_url + '?'
             for param_key in param_keys_sorted:
-                url += urllib.urlencode({param_key: params[param_key]}) + '&'
+                url += urllib.urlencode({param_key: "%s" % (params[param_key])}) + '&'
             url = url[:-1]
             log.debug('Searching for Geonorge datasets: %s', url)
             try:
