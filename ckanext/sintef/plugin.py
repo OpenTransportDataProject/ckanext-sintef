ece7f8f6e8bc9fd216ee70aed633a166251d8a72
Awat A. Langø <awat.langoe@gmail.com>
2016-11-09 13:27:05 +0100

    Removed config updates for public and fanstatic in plugin.py.

--- ckanext/sintef/plugin.py
+++ ckanext/sintef/plugin.py
@@ -9,5 +9,3 @@ class SintefPlugin(plugins.SingletonPlugin):
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'sintef')
